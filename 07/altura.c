d7cb680e5284b2335d7d085532952b028b0aed9a
Pedro Git Projects <pedro.coding.contact@gmail.com>
2021-03-23 14:51:08 -0300

    exercício altura iniciado

---
+++ 07/altura.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (void){
+    
+    double angulo;
+    printf("Insira o valor do ângulo");
+    scanf("%.2lf",&angulo);
+
+    double senoAngulo =  sin(angulo);
+
+    float altura; 
+    float disancia;
+
+    return 0;
+}
